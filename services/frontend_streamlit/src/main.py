537adcb3e142fc5d616e5bb8b6011b6ebe52977b
Eljo <eljo2050@gmail.com>
2025-07-14 08:49:09 -0400

    feat: made chat history scrollable

--- services/frontend_streamlit/src/main.py
+++ services/frontend_streamlit/src/main.py
@@ -3,13 +3,15 @@ import streamlit as st
 st.title("Echo Bot")
 
 if "history" not in st.session_state:
-    st.session_state.history = []
+    st.session_state.history = [] #Array to keep track of history
 
-user_text = st.text_area("You:", "")
 
-if st.button("Send") and user_text:
-    st.session_state.history.append(("You",user_text))
-    st.session_state.history.append(("Bot",user_text))
+with st.form(key="chat_form", clear_on_submit=True): #Input from user cleared from text area after being entered
+    user_text = st.text_input("You:")
+    submit = st.form_submit_button("Send")
+    if submit and user_text:
+        st.session_state.history.append(("You",user_text)) 
+        st.session_state.history.append(("Bot",user_text))
     
 for speaker,text in st.session_state.history:
     if speaker == "You":
