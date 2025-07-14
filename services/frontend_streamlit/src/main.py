ccb60c6949a6d00e64d8d2fc5064104a5bfcd09e
Eljo <eljo2050@gmail.com>
2025-07-14 12:33:09 -0400

    feat: Refactor chat to use st.chat_input

--- services/frontend_streamlit/src/main.py
+++ services/frontend_streamlit/src/main.py
@@ -6,18 +6,18 @@ if "history" not in st.session_state:
     st.session_state.history = [] #Array to keep track of history
 
 
-with st.form(key="chat_form", clear_on_submit=True): #Input from user cleared from text area after being entered
-    user_text = st.text_input("You:")
-    submit = st.form_submit_button("Send")
-    if submit and user_text:
-        st.session_state.history.append(("You",user_text)) 
-        st.session_state.history.append(("Bot",user_text))
+prompt = st.chat_input("Say something")
+if prompt:
+    st.session_state.history.append(("You",prompt))
+    st.session_state.history.append(("Bot",prompt))
     
 for speaker,text in st.session_state.history:
-    if speaker == "You":
-        st.markdown(f"**You:** {text}")
-    else:
-        st.markdown(f"**Bot:** {text}")
+        if speaker == "You":
+            with st.chat_message("user"): 
+                st.markdown(f"**You:** {text}")
+        else:
+            with st.chat_message("bot"): #To differentiate between the user and bot output
+                st.markdown(f"**Bot:** {text}")
 
     
     
