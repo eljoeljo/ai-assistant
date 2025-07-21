ef7ee976b9954e6c6b0a991dcb4836d179e861e1
eljoeljo <eljo2050@gmail.com>
2025-07-20 23:58:55 -0400

    Echobot enhancement (#5)
    * chore: added gitignore
    
    * chore: files for streamlit frontend
    
    * feat: implemented echobot using streamlit
    
    * feat: made chat history scrollable
    
    * feat: Refactor chat to use st.chat_input
    
    * chore: Added svg files for avatar selection
    
    * feat: Implemented SVG avatars for users and bot
    
    * fix: Fixed hardcoded absolute path to be relative instead
    
    ---------
    
    Co-authored-by: justmeloic <loic.muhirwa@gmail.com>
    Co-authored-by: Loïc <145873110+justmeloic@users.noreply.github.com>

--- services/frontend_streamlit/src/main.py
+++ services/frontend_streamlit/src/main.py
@@ -1,21 +1,42 @@
 import streamlit as st
 
+import os
+from pathlib import Path
 st.title("Echo Bot")
 
 if "history" not in st.session_state:
-    st.session_state.history = []
+    st.session_state.history = [] #Array to keep track of history
 
-user_text = st.text_area("You:", "")
 
-if st.button("Send") and user_text:
-    st.session_state.history.append(("You",user_text))
-    st.session_state.history.append(("Bot",user_text))
+prompt = st.chat_input("Say something")
+if prompt:
+    st.session_state.history.append(("You",prompt))
+    st.session_state.history.append(("Bot",prompt))
     
+HERE = Path(__file__).resolve().parent
+ROOT = HERE.parent
+SVG_DIR = ROOT / "svg_files"
+    
+ #Relative Path for AI SVG
+robot_svg = SVG_DIR / "robot-svgrepo-com.svg"
+svg_file_path = str(robot_svg)
+
+    
+ #Relative Path for User SVG
+user_svg = SVG_DIR / "user-svgrepo-com.svg"
+svg_file_path2 = str(user_svg)
+
+
 for speaker,text in st.session_state.history:
-    if speaker == "You":
-        st.markdown(f"**You:** {text}")
-    else:
-        st.markdown(f"**Bot:** {text}")
+        if speaker == "You":
+            with st.chat_message("user",avatar=svg_file_path2):
+                st.markdown(f"{text}")
+        else:
+            with st.chat_message("echobot",avatar=svg_file_path): #To differentiate between the user and bot output
+                st.markdown(f"{text}")
+
+
+
 
     
     
